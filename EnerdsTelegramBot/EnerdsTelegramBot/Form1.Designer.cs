471f2cadc87d3dde454d75718585d77c9f63406e
Ali Salmasi <Alisalmasi@users.noreply.github.com>
2020-01-29 09:35:20 +0330

    First push
    Basic Commands and Form Design Added - first push

---
+++ EnerdsTelegramBot/EnerdsTelegramBot/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace EnerdsTelegramBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgReport = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReport)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnHide);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtToken);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 66);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Token";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(332, 23);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.PasswordChar = '*';
+            this.txtToken.Size = new System.Drawing.Size(438, 24);
+            this.txtToken.TabIndex = 0;
+            this.txtToken.Text = "1007013476:AAGT_P-VxsjZ7RbHREpjgwyn7C2o5KVyBF8";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(6, 24);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(62, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 599);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(52, 17);
+            this.lblStatus.Text = "OFFLINE";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show Token";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgReport
+            // 
+            this.dgReport.AllowUserToAddRows = false;
+            this.dgReport.AllowUserToDeleteRows = false;
+            this.dgReport.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgReport.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dgReport.Location = new System.Drawing.Point(12, 84);
+            this.dgReport.Name = "dgReport";
+            this.dgReport.ReadOnly = true;
+            this.dgReport.Size = new System.Drawing.Size(776, 267);
+            this.dgReport.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "ChatId";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "UserName";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Command";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "MessageID";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Date";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(300, 357);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(233, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(169, 25);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(75, 23);
+            this.btnHide.TabIndex = 3;
+            this.btnHide.Text = "Hide Token";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(74, 24);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(58, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 621);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgReport);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("2  Yekan", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Enerds Telegram Bot";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgReport)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgReport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ EnerdsTelegramBot/EnerdsTelegramBot/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Telegram.Bot;
+using Telegram;
+
+//This Bot Is Developed By Enerds.io
+
+namespace EnerdsTelegramBot
+{
+    public partial class Form1 : Form
+    {
+        static string Token = "";
+        private Thread botThread;
+        Telegram.Bot.TelegramBotClient bot;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Token = txtToken.Text;
+            botThread = new Thread(new ThreadStart(runBot));
+            botThread.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void runBot()
+        {
+            bot = new Telegram.Bot.TelegramBotClient(Token);
+            this.Invoke(new Action(() =>
+                {
+                    lblStatus.Text = "ONLINE";
+                    lblStatus.ForeColor = Color.Green;
+                }));
+            int offSet = 0;
+
+            while (true)
+            {
+                Telegram.Bot.Types.Update[] update = bot.GetUpdatesAsync(offSet).Result;
+
+                foreach (var up in update)
+                {
+                    offSet = up.Id + 1;
+                    if (up.Message == null)
+                        continue;
+
+                    var text = up.Message.Text.ToLower();
+                    var from = up.Message.From;
+                    var chatId = up.Message.Chat.Id;
+
+                    if (text.Contains("/start"))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine(from.Username + "Welcome To our BOT");
+                        sb.AppendLine("You can use the commands below");
+                        sb.AppendLine("About our BOT : /AboutUs");
+                        sb.AppendLine("Contact US : /Contact");
+                        sb.AppendLine("Address : /Address");
+                        bot.SendTextMessageAsync(chatId, sb.ToString());
+                    }
+                    // yademan bashad hatman az horof kochak estefade shavad be in elat .tolower estefade shode dar line 61 ***
+
+                    else if (text.Contains("/aboutus"))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine("Our Vision is to help streamers to build  theire career with ease");
+                        bot.SendTextMessageAsync(chatId, sb.ToString());
+                    }
+                    else if (text.Contains("/contact"))
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine("contact us at info@enerds.io");
+                        bot.SendTextMessageAsync(chatId, sb.ToString());
+                    }
+                    else if (text.Contains("/address"))
+                    {
+                        string website = "https://www.enerds.io";
+                        StringBuilder sb = new StringBuilder();
+                        sb.AppendLine($"Visit our website {website}");
+                        bot.SendTextMessageAsync(chatId, sb.ToString());
+                    }
+
+                    dgReport.Invoke(new Action(()=>
+                        {
+                            dgReport.Rows.Add(chatId, from.Username, text, up.Message.MessageId, up.Message.Date.ToString("yyyy/MM/dd - HH:MM"));
+                    }));
+
+                }
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            botThread.Abort();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtToken.PasswordChar = '\0';
+        }
+
+        private void btnHide_Click(object sender, EventArgs e)
+        {
+            txtToken.PasswordChar = '*';
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            botThread.Abort();
+        }
+    }
+}
